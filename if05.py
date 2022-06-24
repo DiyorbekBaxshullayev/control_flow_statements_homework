78871f9dc316d2bfeec9e16fd8f9f6b14d4d82ac
Diyorbek <baxshullayevd4@gmail.com>
2022-06-24 12:12:20 +0500

    5

--- if05.py
+++ if05.py
@@ -8,4 +8,20 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    s=""
+
+    if a<0 and b<0 and c<0:
+        s=3
+
+    elif (a>0 and b<0 and c<0) or (a<0 and b>0 and c<0) or (a<0 and b<0 and c>0):
+        s=2
+
+    elif (a>0 and b>0 and c<0) or (a>0 and b<0 and c>0) or (a<0 and b>0 and c>0):
+        s=1
+
+    else:
+        s=0
+    
+    return s
+
+print(main(-5,-2,2))
